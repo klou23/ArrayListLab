652240cc3c7261fc85b1293f3f23a67a33746928
Kevin Lou <58053969+klou23@users.noreply.github.com>
2021-10-28 08:38:37 -0600

    Update Node class

--- src/Node.java
+++ src/Node.java
@@ -1,64 +1,44 @@
 public class Node<T>{//using type T syntax
 
-    private Node nodeOne;
-    private Node nodeTwo;
+    private Node<T> child;
+    private Node<T> parent;
 
     private T value;
 
-    public Node(){//default constructor
-        nodeOne = null;
-        nodeTwo = null;
-    }
+    public Node(){}
 
     public Node(T value){
-        nodeOne = null;
-        nodeTwo = null;
         this.value = value;
     }
 
-    public Node(T value, Node node1, Node node2){//constructor with value node1 and node2
-        this.nodeOne = node1.nodeOne;
-        this.nodeTwo = node2.nodeTwo;
+    public Node(T value, Node<T> child, Node<T> parent){//constructor with value node1 and node2
+        this.child = child;
+        this.parent = parent;
         this.value = value;
     }
 
-
-    //Mutators
-
-    public void setValue(T value){
-        this.value = value;
+    public Node<T> getChild() {
+        return child;
     }
 
-    public void setFirstNode(Node nodeOne){// sets the first node
-        this.nodeOne = nodeOne;
-        makeChild(nodeOne);
+    public void setChild(Node<T> child) {
+        this.child = child;
     }
 
-    public void setSecondNode(Node nodeTwo) {// sets the second node
-        this.nodeTwo = nodeTwo;
-        makeParent(nodeTwo);
+    public Node<T> getParent() {
+        return parent;
     }
 
-    private void makeChild(Node nodeOne){//child node
-        this.nodeOne = nodeOne;
+    public void setParent(Node<T> parent) {
+        this.parent = parent;
     }
 
-    private void makeParent(Node nodeTwo){//parent node
-        this.nodeTwo = nodeTwo;
-    }
-
-    //accessors
-
-    public T getValue(){
+    public T getValue() {
         return value;
     }
 
-    public Node getFirstNode(){//gets the first node
-        return nodeOne;
-    }
-
-    public Node getSecondNode(){//gets the second node
-        return nodeTwo;
+    public void setValue(T value) {
+        this.value = value;
     }
 
     public String toString(){//to string method
@@ -66,7 +46,7 @@ public class Node<T>{//using type T syntax
         else return "Node: "+ value.toString();
     }
 
-    public boolean equals(Node node){
+    public boolean equals(Node<T> node){
         if (this.value == null && node.value == null){
             return true;
         } else if (this.value == null || node.value == null){
